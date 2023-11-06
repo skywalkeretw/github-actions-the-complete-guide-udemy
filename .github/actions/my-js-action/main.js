d25643df3be40e98ea37e15e53d5585c49acd461
Luke Roy <lukeroy@outlook.com>
2023-11-06 17:10:37 +0100

    16 js  input

--- .github/actions/my-js-action/main.js
+++ .github/actions/my-js-action/main.js
@@ -3,7 +3,9 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
-    core.notice("hello from nodej")
+    yourname = core.getInput('name', {required: true})
+    yourage = core.getInput('age', {required: true})
+    core.notice("hello " + yourname + " your age is: " +yourage)
 }
 
 run()
